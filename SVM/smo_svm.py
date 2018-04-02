b419f6a4652605ae793a0a45ae2d121b0a5f8716
GuanglinZhou <GuanglinZhou@users.noreply.github.com>
2018-04-02 12:44:56 +0800

    update SVM and SMO code

--- SVM/smo_svm.py
+++ SVM/smo_svm.py
@@ -3,14 +3,13 @@
 # @Date    : 2018-03-29 17:06:57
 # @Author  : guanglinzhou (xdzgl812@163.com)
 # @Link    : https://github.com/GuanglinZhou
-# @Version : $Id$
 import numpy as np
-from time import sleep
 from random import shuffle
 import matplotlib.pyplot as plt
 import math
 
 
+# 加载数据集
 def loadDataSet(fileName):
     dataMat = []
     labelMat = []
@@ -24,6 +23,7 @@ def loadDataSet(fileName):
     return dataMat, labelMat
 
 
+# 定义一个类作为数据结构，存储一些关键值
 class classSMO:
     def __init__(self, dataMat, labelMat, C, tol):  # Initialize the structure with the parameters
         self.dataMat = dataMat
@@ -34,7 +34,8 @@ class classSMO:
         self.alphas = np.mat(np.zeros((self.m, 1)))
         self.b = 0
         self.eCache = np.mat(np.zeros((self.m, 1)))  # first column is valid flag
-        self.kernelType = 'Gaussian'
+        self.kernelType = 'linear'
+        # self.kernelType = 'Gaussian'
         # 储存K(xi,xj)的值
         self.kernelFunctionValue = np.mat(np.zeros((self.m, self.m)))
         for i in range(self.m):
@@ -45,7 +46,7 @@ def kernel(i, objectSMO):
     if (objectSMO.kernelType == 'linear'):
         return np.dot(objectSMO.dataMat[:, :], objectSMO.dataMat[i, :].transpose())
     if (objectSMO.kernelType == 'Gaussian'):
-        sigma = 10
+        sigma = 1
         temp = -np.square(objectSMO.dataMat[:, :] - objectSMO.dataMat[i, :]) / (2 * math.pow(sigma, 2))
         ndarr = np.array(temp)
         ndarr = np.sum(ndarr, axis=1)
@@ -53,17 +54,40 @@ def kernel(i, objectSMO):
         return np.exp(temp)
 
 
+# 计算g(xi)的值
+def computeGx(i, objectSMO):
+    if (objectSMO.kernelType == 'linear'):
+        g_xi1 = np.multiply(objectSMO.alphas, objectSMO.labelMat)
+        g_xi2 = np.multiply(objectSMO.dataMat, objectSMO.dataMat[i])
+        g_xi = np.sum(np.multiply(g_xi1, g_xi2)) + objectSMO.b
+    elif (objectSMO.kernelType == 'Gaussian'):
+        g_xi = np.multiply(objectSMO.alphas, objectSMO.labelMat)
+        g_xi = np.multiply(g_xi, objectSMO.kernelFunctionValue[:, i])
+        g_xi = np.sum(g_xi) + objectSMO.b
+    else:
+        g_xi = 0
+    return g_xi
+
+
+# 计算Ei的值
 def computeEk(k, objectSMO):
     print('computeEi')
-    g_xi1 = np.multiply(objectSMO.alphas, objectSMO.labelMat)
-    print('g_xi1.shape')
-    print(g_xi1.shape)
-    g_xi2 = np.multiply(objectSMO.dataMat, objectSMO.dataMat[k])
-    print(g_xi2.shape)
-    g_xi = np.sum(np.multiply(g_xi1, g_xi2)) + objectSMO.b
+    g_xi = computeGx(k, objectSMO)
     return (g_xi - objectSMO.labelMat[k]).item()
 
 
+# 检查误分类的个数
+def checkClassifierErrorSample(objectSMO):
+    errorNum = 0
+    for i in range(objectSMO.dataMat.shape[0]):
+        predi = np.sign(computeGx(i, objectSMO))
+        yi = objectSMO.labelMat[i].item()
+        if (predi != yi):
+            errorNum += 1
+    print('核函数为：{}, 样本总数为：{}，分类错误的样本个数为：{}'.format(objectSMO.kernelType, objectSMO.m, errorNum))
+
+
+# 绘制原始数据
 def plotData(Xmat, ymat):
     Xarray = np.array(Xmat)
     yarray = np.array(ymat)
@@ -71,28 +95,23 @@ def plotData(Xmat, ymat):
     plt.figure()
     for i in range(Xarray.shape[0]):
         plt.plot(Xarray[i, 0], Xarray[i, 1], col[yarray[i][0]] + 'o')
-    # plt.ylim([-1.5, 1.5])
     plt.show()
 
 
+# 更新Ei
 def updateEk(objectSMO, k):
-    print('\n\n')
-    print('before update,Ek: {}'.format(objectSMO.eCache[k]))
     objectSMO.eCache[k] = computeEk(k, objectSMO)
-    print('k: {}, updateEk: {}'.format(k, objectSMO.eCache[k]))
 
 
+# 更新alpha1和alpha2
 def takeStep(i, j):
     if (i == j):
         return 0
-    # alpha1 = objectSMO.alphas[i]
-    print('第一个变量下标{}，第二个变量下标{}'.format(i, j))
     # y1 y2 E1 E2为标量形式
     y1 = objectSMO.labelMat[i].item()
     y2 = objectSMO.labelMat[j].item()
     E1 = computeEk(i, objectSMO)
     E2 = computeEk(j, objectSMO)
-    print('E1: {},E2: {}'.format(E1, E2))
     # alpha1old alpha2old为标量形式
     alpha1old = objectSMO.alphas[i].item()
     alpha2old = objectSMO.alphas[j].item()
@@ -105,16 +124,13 @@ def takeStep(i, j):
         H = min(C, alpha2old + alpha1old)
     if (L == H):
         return 0
-    print('L: {},H: {}'.format(L, H))
     # Kij为标量形式
     K11 = objectSMO.kernelFunctionValue[1, 1]
     K22 = objectSMO.kernelFunctionValue[2, 2]
     K12 = objectSMO.kernelFunctionValue[1, 2]
     eta = K11 + K22 - 2 * K12
-    print('eta:{}'.format(eta))
     if (eta > 0):
         alpha2New = alpha2old + y2 * (E1 - E2) / eta
-        print('alpha2New: {}'.format(alpha2New))
         if (alpha2New < L):
             alpha2New = L
         elif (alpha2New > H):
@@ -137,11 +153,10 @@ def takeStep(i, j):
     return 1
 
 
+# 选择第2个变量
 def secondChoice(i1, objectSMO):
     maxAbsEDelta = 0
     i2OfMaxAbsEDelta = 0
-    print('i1: {}'.format(i1))
-    print('objectSMO.eCache[i1]: {}'.format(objectSMO.eCache[i1]))
     Ei1 = objectSMO.eCache[i1].item()
     for i2 in range(objectSMO.eCache.shape[0]):
         Ei2 = objectSMO.eCache[i2].item()
@@ -181,6 +196,7 @@ def examineExample(i1, objectSMO, C):
     return 0
 
 
+# 外循环
 def mainRoutine(dataMat, C, maxIter):
     m, n = np.shape(dataMat)
     numChanged = 0
@@ -205,17 +221,18 @@ def mainRoutine(dataMat, C, maxIter):
             examineAll = True
 
 
+# 计算linear的权重
 def computeTheta(objectSMO):
     theta = np.dot(np.multiply(objectSMO.alphas, objectSMO.labelMat).transpose(), objectSMO.dataMat)
     b = objectSMO.b
     thetaList = theta.tolist()
     thetaList[0].append(b)
-    theta = np.mat(thetaList)
-    return theta.transpose()
+    theta = np.mat(thetaList).transpose()
+    return theta
 
 
-def plotHyperplaneLinear(Xmat, ymat, theta):
-    print(theta.shape)
+# 绘制核为linear的超平面
+def plotHyperplaneLinear(Xmat, ymat, theta, objectSMO):
     m, n = np.shape(Xmat)
     Xarray = np.array(Xmat)
     yarray = np.array(ymat)
@@ -223,46 +240,31 @@ def plotHyperplaneLinear(Xmat, ymat, theta):
     plt.figure()
     for i in range(Xarray.shape[0]):
         plt.plot(Xarray[i, 0], Xarray[i, 1], col[yarray[i][0]] + 'o')
-    # plt.ylim([-6, 6])
+    plt.ylim([-6, 6])
     plt.plot(Xarray[:, 0],
              (-(theta[0][0] * Xarray[:, 0] + np.multiply(theta[2][0], np.ones((m, 1)))) / theta[1][0]).transpose(),
              c='g')
+    # 标出支持向量
+    for i in range(objectSMO.m):
+        if (round(objectSMO.alphas[i].item(), 3) < objectSMO.C and round(objectSMO.alphas[i].item(), 3) > 0):
+            plt.plot(Xarray[i, 0], Xarray[i, 1], 'y' + 'o')
     plt.show()
 
 
-# def plotHyperplaneRBF(objectSMO):
-#     m, n = np.shape(objectSMO.Xmat)
-#     Xarray = np.array(objectSMO.Xmat)
-#     yarray = np.array(objectSMO.ymat)
-#     col = {+1: 'r', -1: 'b'}
-#     plt.figure()
-#     for i in range(Xarray.shape[0]):
-#         plt.plot(Xarray[i, 0], Xarray[i, 1], col[yarray[i][0]] + 'o')
-#     # plt.ylim([-6, 6])
-#
-#     plt.plot(Xarray[:, 0],
-#              (-(theta[0][0] * Xarray[:, 0] + np.multiply(theta[2][0], np.ones((m, 1)))) / theta[1][0]).transpose(),
-#              c='g')
-#     plt.show()
-
-
 if __name__ == '__main__':
-    fileName = 'testSetRBF.txt'
+    '''
+    通过更改此处文件名，以及类别classSMO中成员kernelType，切换linear kernel和RBF kernel
+    '''
+    fileName = 'testSetLinear.txt'
+    # fileName = 'testSetRBF.txt'
     dataMat, labelMat = loadDataSet(fileName)
     print(np.shape(dataMat))
     C = 200
     tol = 0.001
     maxIter = 200
     objectSMO = classSMO(dataMat, labelMat, C, tol)
-    objectSMO.kernelType = 'Gaussian'
-    # objectSMO.kernelType = 'linear'
     mainRoutine(dataMat, C, maxIter)
-    print('\n\n\n')
-    for alpha in objectSMO.alphas:
-        print(alpha)
-    theta = computeTheta(objectSMO)
-    # print(theta)
-    # theta = np.mat([0.80597085, - 0.27600769, - 3.78539333]).transpose()
-    # print('theta.shape: {}'.format(theta.shape))
     plotData(objectSMO.dataMat, objectSMO.labelMat)
-    # plotHyperplaneLinear(objectSMO.dataMat, objectSMO.labelMat, theta)
+    theta = computeTheta(objectSMO)
+    plotHyperplaneLinear(objectSMO.dataMat, objectSMO.labelMat, theta, objectSMO)
+    checkClassifierErrorSample(objectSMO)
