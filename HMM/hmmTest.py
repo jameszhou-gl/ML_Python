60323331a8896a344ea4c7e877175c0fbaf62658
guanglinzhou <xdzgl812@163.com>
2018-06-19 20:04:33 +0800

    update SVM and SMO code

---
+++ HMM/hmmTest.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-03-23 13:42:03
+# @Author  : guanglinzhou (xdzgl812@163.com)
+# @Link    : https://github.com/GuanglinZhou
+# @Version : $Id$
+
+
+import numpy as np
+from hmmlearn import hmm
+
+states = ["box 1", "box 2", "box3"]
+n_states = len(states)
+
+observations = ["red", "white"]
+n_observations = len(observations)
+
+start_probability = np.array([0.2, 0.4, 0.4])
+
+transition_probability = np.array([
+    [0.5, 0.2, 0.3],
+    [0.3, 0.5, 0.2],
+    [0.2, 0.3, 0.5]
+])
+
+emission_probability = np.array([
+    [0.5, 0.5],
+    [0.4, 0.6],
+    [0.7, 0.3]
+])
+
+model = hmm.MultinomialHMM(n_components=n_states)
+model.startprob_ = start_probability
+model.transmat_ = transition_probability
+model.emissionprob_ = emission_probability
+
+seen = np.array([[0, 1, 0]]).T
+logprob, box = model.decode(seen, algorithm='viterbi')
+
+# print("The ball picked:", ", ".join(map(lambda x: observations[x], seen)))
+print("The hidden box: ", ", ".join(map(lambda x: states[x], box)))
